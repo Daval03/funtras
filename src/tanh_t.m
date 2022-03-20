2819daa0d4d22f142c99112d3a9b684fda8e5cd5
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2022-03-20 16:30:26 -0600

    Validacion para tangente hiperbolica

--- src/tanh_t.m
+++ src/tanh_t.m
@@ -1,3 +1,10 @@
 function Sk = tanh_t (x)
-  Sk =sinh_t(x)*div_t(cosh_t(x));
+  if (x>15)
+    Sk=1;
+  elseif (x<15)
+    Sk=-1;
+  else
+    Sk =sinh_t(x)*div_t(cosh_t(x));  
+  endif
+  
 endfunction
